9521f677f819afc1004f16e96215b5de43291ca9
sweet04 <gozalovash@gmail.com>
2019-12-03 20:44:28 +0400

    added html files and servlets, will be fixed

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -1,22 +1,29 @@
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter w = resp.getWriter();
-        w.println("HelloWorld!");
-        w.close();
+        Path path = Paths.get("./content/user.html");
+        ServletOutputStream os = resp.getOutputStream();
+        Files.copy(path, os);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer =resp.getWriter();
-        writer.println("HelloWorld.POST");
-        writer.close();
+        //Path path = Paths.get("./content/user.html");
+        //req.getParameter()
+        //PrintWriter writer =resp.getWriter();
+        //writer.println("HelloWorld.POST");
+        //writer.close();
+        //resp.sendRedirect("localhost/content/user.html");
     }
 }
--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -6,7 +6,8 @@ public class WebServer {
     public static void main(String[] args) throws Exception {
         Server server =new Server(8082);
         ServletContextHandler handler=new ServletContextHandler();
-        handler.addServlet(HelloServlet.class,"/*");
+        handler.addServlet(HelloServlet.class,"/users");
+        handler.addServlet(LoginServlet.class, "/*");
 
         server.setHandler(handler);
         server.start();
