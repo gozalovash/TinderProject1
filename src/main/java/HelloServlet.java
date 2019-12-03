5cc476b486374c2776c0f7e6d2e66595bebcd5d2
sweet04 <gozalovash@gmail.com>
2019-12-03 09:15:50 +0400

    Launching of Web-Server

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter w = resp.getWriter();
+        w.println("HelloWorld!");
+        w.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer =resp.getWriter();
+        writer.println("HelloWorld.POST");
+        writer.close();
+    }
+}
---
+++ src/main/java/WebServer.java
@@ -0,0 +1,18 @@
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class WebServer {
+    public static void main(String[] args) throws Exception {
+        Server server =new Server(8082);
+        ServletContextHandler handler=new ServletContextHandler();
+        handler.addServlet(HelloServlet.class,"/*");
+
+        server.setHandler(handler);
+        server.start();
+        server.join();
+
+
+
+    }
+}
