e304e4ab9246633cc614405644cdb495666a7fd3
sweet04 <gozalovash@gmail.com>
2019-12-04 20:51:00 +0400

    auth, authService--will be changed to check with tables

---
+++ src/main/java/service/Auth.java
@@ -0,0 +1,5 @@
+package service;
+
+public interface Auth {
+    boolean check(String username, String password);
+}
--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -1,19 +1,42 @@
 package servlets;
 
+import service.Auth;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class LoginServlet extends HttpServlet {
+    private final Auth auth;
+
+    public LoginServlet(Auth auth) {
+        this.auth = auth;
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        Path path = Paths.get("./content/login.html");
+        ServletOutputStream os = resp.getOutputStream();
+        Files.copy(path, os);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String user_name=req.getParameter("user_name");
+        String user_password=req.getParameter("user_passwd");
+        boolean checked=auth.check(user_name, user_password);
+
+        System.out.println(user_name);
+        System.out.println(user_password);
+        try(PrintWriter w = resp.getWriter()) {
+            w.println("LoginServlet.POST");
+            w.printf("user:%s %s\n",user_name, checked ? "logged in successfully" : "login failed");
+        };//will be changed later-- redirect to users page
     }
 }
