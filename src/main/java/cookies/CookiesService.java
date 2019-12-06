4218a3e5122dcadcd62a3dc285d2d142c53f690b
Lala <hajiyeva.lalah@gmail.com>
2019-12-06 13:48:35 +0400

    -- added cookieService

---
+++ src/main/java/cookies/CookiesService.java
@@ -0,0 +1,45 @@
+package cookies;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+public class CookiesService {
+    private String cookieName = "onlineUser";
+    private HttpServletResponse response;
+    HttpServletRequest request;
+
+    public CookiesService(HttpServletResponse response, HttpServletRequest request) {
+        this.response = response;
+        this.request = request;
+    }
+
+    public Cookie getCookies() {
+        Cookie cookie = null;
+        Cookie cookies[] = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals(cookieName)) {
+                    cookie = c;
+                }
+            }
+
+        }
+        return cookie;
+    }
+
+    public void saveCookies(int value) {
+        response.addCookie(new Cookie(cookieName, String.valueOf(value)));
+    }
+
+
+    public void deleteCookies() {
+        Arrays.stream(request.getCookies()).
+                filter(c -> c.getName().equalsIgnoreCase(cookieName)).
+                map(c -> new Cookie(c.getName(), c.getValue()) {{
+                    setMaxAge(0);
+                }}).forEach(response::addCookie);
+    }
+
+}
