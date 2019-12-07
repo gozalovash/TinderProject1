0a05668a2b46a2001395cf2ef28ff9bb90f59c43
Lala <hajiyeva.lalah@gmail.com>
2019-12-08 01:39:41 +0400

    -- modify methods in daoUser

--- src/main/java/dao/DaoUserSql.java
+++ src/main/java/dao/DaoUserSql.java
@@ -21,26 +21,24 @@ public class DaoUserSql implements Dao<User> {
     @Override
     public User get(int id) {
         User user = null;
-        String SQLS = "SELECT users.username , users.name , users.surname , users.password , users.imgurl WHERE id =?";
+        String SQLS = "SELECT users.username , users.name , users.surname , users.imgurl WHERE id =?";
         try {
             PreparedStatement statement = connection.prepareStatement(SQLS);
-                statement.setInt(1, id);
-                statement.execute();
-                ResultSet resultSet = statement.executeQuery();
-                while ((resultSet.next())){
-                  String userName = resultSet.getString("username");
-                    String Name = resultSet.getString("name");
-                    String Surname = resultSet.getString("surname");
-                    String password = resultSet.getString("password");
-                    String ImgUrl = resultSet.getString("imgurl");
-                    user = new User(id,userName,Name,Surname,password,ImgUrl);
-                }
-            } catch (SQLException ex) {
+            statement.setInt(1, id);
+            statement.execute();
+            ResultSet resultSet = statement.executeQuery();
+            while ((resultSet.next())) {
+                String userName = resultSet.getString("username");
+                String Name = resultSet.getString("name");
+                String Surname = resultSet.getString("surname");
+                String ImgUrl = resultSet.getString("imgurl");
+                user = new User(id, userName, Name, Surname, ImgUrl);
+            }
+        } catch (SQLException ex) {
             ex.printStackTrace();
         }
         return user;
     }
-
 
 
     @Override
@@ -70,18 +68,47 @@ public class DaoUserSql implements Dao<User> {
         }
         return user;
     }
+    public User getByUserId(User item) {
+        User user = null;
+        String SQLS = "SELECT FROM users WHERE id = ?";
+        try {
+            PreparedStatement stm = connection.prepareStatement(SQLS);
+            stm.setInt(1, user.getUserId());
+            stm.execute();
+            ResultSet resultSet = stm.executeQuery();
+            if (resultSet.next()) {
+                String nickName = resultSet.getString("username");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                String password = resultSet.getString("password");
+                String photoUrl = resultSet.getString("imgUrl");
+                user = new User( nickName, name, surname, password, photoUrl);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
 
     @Override
     public void save(User item) {
-        String SQLI = "INSERT INTO users(id,username,name,password,imgurl) VALUES(?,?,?,?)";
+        String SQLI = "INSERT INTO users(id,username,name,surname,password,imgurl) VALUES(?,?,?,?,?,?)";
+        try {
+            PreparedStatement statement = connection.prepareStatement(SQLI);
+            statement.setInt(1, item.getUserId());
+            statement.setString(2, item.getNickName());
+            statement.setString(3, item.getUserName());
+            statement.setString(4, item.getUserSurname());
+            statement.setString(5, item.getPassword());
+            statement.setString(6, item.getPhotoUrl());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
     }
 
-    public User getOtherUsers(int userId) {
-        //implement
-        return null;
-    }
 
     @Override
     public void delete(int id) {
@@ -92,5 +119,9 @@ public class DaoUserSql implements Dao<User> {
     @Override
     public void update(User item) {
 
+    }
+
+    public User getOtherUsers(int otherUserId) {
+        return  null;
     }
 }
